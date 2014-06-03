3bc0505a9a9a8ffc8e5406d86205adc91abe5649
wolfblood316 <naruto10>
2014-06-03 13:50:58 -0400

    added php file

---
+++ lab9/includes/nav.php
@@ -0,0 +1,6 @@
+ <ul class="nav">
+      <li><a href="#">Link one</a></li>
+      <li><a href="#">Link two</a></li>
+      <li><a href="#">Link three</a></li>
+      <li><a href="#">Link four</a></li>
+    </ul>
